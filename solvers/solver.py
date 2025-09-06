26f71eba6aa5d745452a7c6fbdbe3d5729df65ea
Alex Oliver <emmet2by4@gmail.com>
2025-09-06 09:59:45 +0100

    Update solver.py
    Optimised the solver

--- solvers/solver.py
+++ solvers/solver.py
@@ -4,14 +4,11 @@ import pyperclip
 import pyautogui
 
 with open("path/to/dictionary.txt", encoding="utf-8") as file:
-    lines = [line.rstrip() for line in file]
-
-reference = []
-for line in lines:
-    if line:
-        reference.append(line)
+    reference = [line.rstrip() for line in file]
 
 positions = [[0, 0], [1, 0], [2, 0], [3, 0], [0, 1], [1, 1], [2, 1], [3, 1], [0, 2], [1, 2], [2, 2], [3, 2]]
+instructions = []
+pyautogui.PAUSE = .085
 
 pyautogui.click()
 pyautogui.hotkey("ctrlleft", "a")
@@ -20,8 +17,6 @@ pyautogui.hotkey("ctrlleft", "c")
 s = pyperclip.paste()
 game = s.split("\r\n")
 game = game[7:len(game)-1]
-
-instructions = []
 
 for i in range(12):
     j = reference.index(game[i])
@@ -32,16 +27,16 @@ for i in range(12):
         end = j-1
     
     append = not any(game.index(reference[end]) in jtem for jtem in instructions)
-    
+
     if append:
         instructions.append([game.index(game[i]), game.index(reference[end])])
 
 pyautogui.moveTo(390, 310, duration = 0)
 for i in range(len(instructions)):
-    x, y = positions[instructions[i][0]][0], positions[instructions[i][0]][1]
-    pyautogui.moveTo(390+(x*370), 310+(y*280), duration = 0)
-
+    a = positions[instructions[i][0]]
+    pyautogui.moveTo(390+(a[0]*370), 310+(a[1]*280), duration = 0)
     pyautogui.click()
-    x, y = positions[instructions[i][1]][0], positions[instructions[i][1]][1]
-    pyautogui.moveTo(390+(x*370), 310+(y*280), duration = 0)
+
+    a = positions[instructions[i][1]]
+    pyautogui.moveTo(390+(a[0]*370), 310+(a[1]*280), duration = 0)
     pyautogui.click()
